13a12490fb2b4ed2e6d8bb7b5cc5bde9a120240b
Theshika Navod <theshikanavod@gmail.com>
2024-12-06 12:09:16 +0530

    get,update,delete todos compleated

--- src/main/java/com/todo/todo/app/controller/TodoController.java
+++ src/main/java/com/todo/todo/app/controller/TodoController.java
@@ -2,12 +2,12 @@ package com.todo.todo.app.controller;
 
 import com.todo.todo.app.dto.TodoDto;
 import com.todo.todo.app.service.TodoService;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/todo")  // API endpoint for creating a new todo item
@@ -21,6 +21,31 @@ public class TodoController {
         TodoDto savedTodoDTO =todoService.addTodo(todoDto);
         return ResponseEntity.ok(savedTodoDTO);
 
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TodoDto>> getAllTodo(){
+       List<TodoDto> savedList= todoService.getAllTodos();
+       return ResponseEntity.ok(savedList);
+
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TodoDto> getTodoById(@PathVariable Long id){
+        TodoDto todoDto = todoService.getTodoById(id);
+        return ResponseEntity.ok(todoDto);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<TodoDto> updateTodo(@PathVariable("id") Long id, @RequestBody TodoDto todoDto){
+        TodoDto updatedTodoDto = todoService.updateTodo(id, todoDto);
+        return ResponseEntity.ok(updatedTodoDto);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteTodo(@PathVariable("id") Long id){
+        todoService.deleteTodo(id);
+        return ResponseEntity.ok("Delete successfully" +id);
     }
 
 
--- src/main/java/com/todo/todo/app/service/TodoService.java
+++ src/main/java/com/todo/todo/app/service/TodoService.java
@@ -2,10 +2,14 @@ package com.todo.todo.app.service;
 
 import com.todo.todo.app.dto.TodoDto;
 import com.todo.todo.app.entity.Todo;
+import com.todo.todo.app.exception.ResourceNotFoundException;
 import com.todo.todo.app.mapper.TodoMapper;
 import com.todo.todo.app.reopsitory.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.todo.todo.app.mapper.TodoMapper.todoDtoToEntity;
 
@@ -25,7 +29,31 @@ public class TodoService {
 
     }
 
+    public List<TodoDto> getAllTodos(){
+        List<Todo> todoList = todoRepository.findAll();
+        return todoList.stream().map(TodoMapper::entityToTodoDto)
+                .collect(Collectors.toList());
+
+    }
 
 
+    public TodoDto getTodoById(Long id) {
+        Todo todo = todoRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Todo not found" + id));
+        return TodoMapper.entityToTodoDto(todo);
+    }
 
+    public TodoDto updateTodo(Long id, TodoDto todoDto) {
+        Todo todo = todoRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Todo not found" + id));
+        todo.setTitle(todoDto.getTitle());
+        todo.setDescription(todoDto.getDescription());
+        todo.setCompleted(todoDto.isCompleted());
+
+        Todo updatedTodo = todoRepository.save(todo);
+        return TodoMapper.entityToTodoDto(updatedTodo);
+    }
+
+    public void deleteTodo(Long id) {
+        Todo todo = todoRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Todo not found" + id));
+        todoRepository.delete(todo);
+    }
 }
